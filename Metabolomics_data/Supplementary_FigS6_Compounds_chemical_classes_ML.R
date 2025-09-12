87838195e5b25e20d028cf3b6c7bb3c402c484c5
Ghiwa Makke <124733108+Ghiwa-Makke@users.noreply.github.com>
2025-09-12 15:51:44 -0700

    Add files via upload

---
+++ Metabolomics_data/Supplementary_FigS6_Compounds_chemical_classes_ML.R
@@ -0,0 +1,83 @@
+### LEO 2022 Surface Samples Metabolomics - RP 
+### Machine Learning discriminatiry Features 
+### ML- classes 
+
+# 1. Importing Libraries
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(ggplot2)
+  library(dplyr)
+}) 
+
+
+# 2. Import data
+
+project_dir <- getwd()
+project_name <- 'LEO_RP'
+
+# Load compounds table
+
+Bare <- file.path('Input_files/ML/Bare_ML_ft.csv')
+Bare <- read_csv(Bare)
+Bare<-Bare %>%
+  rename(FeatureID = Bare_ML_ft)
+
+Biocrust <- file.path('Input_files/ML/Biocrust_ML_ft.csv')
+Biocrust <- read_csv(Biocrust)
+Biocrust<-Biocrust %>%
+  rename(FeatureID = Biocrust_ML_ft)
+
+Moss <- file.path('Input_files/ML/Moss_ML_ft.csv')
+Moss <- read_csv(Moss)
+Moss<-Moss %>%
+  rename(FeatureID = Moss_ML_ft)
+
+classification <- read.csv("Input_Files/RP_Compound_Inchikey_classifications.csv") %>%
+  rename(Superclass = Final_Superclass)
+classification$Superclass[is.na(classification$Superclass)] <- "Unclassified"
+
+Bare_class <- merge(Bare, classification, by= "FeatureID")
+Biocrust_class <- merge(Biocrust, classification, by= "FeatureID")
+Moss_class <- merge(Moss, classification, by= "FeatureID")
+
+
+# Combine data frames
+Bare_class$Source<- 'Bare'
+Biocrust_class$Source<- 'Biocrust'
+Moss_class$Source<- 'Moss'
+
+combined_df_metabo <- rbind(Bare_class, Biocrust_class, Moss_class)
+
+# Count the number of compounds per classification for each sample type
+count_df_metabo <- combined_df_metabo %>%
+  group_by(Source, Superclass) %>%
+  summarise(NumFeatures = n()) %>%
+  ungroup()
+
+# Create the bubble plot with different colors for each sample type
+Metabolite_class <- ggplot(count_df_metabo, aes(x = Source, y = Superclass)) +
+  geom_point(aes(size = NumFeatures, fill = Source), shape = 21, color = "black", alpha = 0.7) +
+  scale_size_continuous(range = c(3, 8), breaks = seq(1, max(count_df_metabo$NumFeatures), by = 2)) +
+  scale_fill_manual(values = c("Bare" = '#3d1d0a', "Biocrust" = '#a8581b', "Moss" = '#116311')) +
+  theme_minimal(base_size = 8) +
+  labs(
+    #title = "Number of Important Features by Sample Type and Classification",
+    x = "",
+    y = "Compound Classification",
+    size = "Number of Features",
+    fill = "Sample Type"
+  ) +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    axis.title = element_text(face = "bold", size = 8),
+    axis.text = element_text(size = 8),
+    legend.position = "right",
+    panel.grid.minor = element_blank(),
+    legend.box = "horizontal",
+    legend.text = element_text(size = 8)
+  )+ guides(fill = guide_legend(ncol = 1)) 
+
+Metabolite_class
+
+# Save the combined plot
+ggsave("output_figures/ML_Bubble_Plot_Metabo.svg", Metabolite_class, dpi = 300, height = 2.5, width = 6)
---
+++ Metabolomics_data/Supplementary_Figs_S4-S5-Overall_Classes_Pathways_Heatmaps.R
@@ -0,0 +1,245 @@
+## LEO Surface samples - Metabolomics Data Analyis
+## Compound Classes Heatmap (Supplementary figure S4)
+## Kegg Pathway Heatmap (Supplementary Figure S5)
+
+
+# import_Library ---------
+library(tidyverse)
+library(RColorBrewer)
+library(pheatmap)
+library(gridExtra)
+library(grid)
+library(readxl)
+
+# 2. Import data ---------
+
+project_dir <- getwd()
+
+RP_norm <- read.csv("Input_files/RP_norm_pareto.csv", row.names = 1, check.names = F)%>%
+  t()%>%
+  as.data.frame()
+  
+RP_stand <- scale(RP_norm) %>%
+  t()%>%
+  as.data.frame()%>%
+  rownames_to_column(var = "SampleID")
+
+HILIC_norm <- read.csv("Input_files/HILIC_norm_pareto.csv", row.names = 1, check.names = F)%>%
+  t()%>%
+  as.data.frame()
+
+HILIC_stand <- scale(HILIC_norm) %>%
+  t()%>%
+  as.data.frame()%>%
+  rownames_to_column(var = "SampleID")
+
+Norm_full_matrix <- merge(RP_stand,HILIC_stand, by = "SampleID")%>%
+  column_to_rownames(var = "SampleID")
+
+# Load compounds table
+norm_matrix_stand <- as.data.frame(t(Norm_full_matrix))%>% 
+  rownames_to_column(var = "FeatureID")
+
+# compounds_table <- file.path(tables_dir, 'gap_filled_compounds_table.csv')
+# compounds_table <- read_csv(compounds_table)
+
+# Import metadata and fix names
+metadata <- read.csv("Input_files/fixed_metadata.csv")
+
+metadata <- metadata %>%
+  arrange(Type)
+
+RP_classification <- read.csv("Input_Files/RP_Compound_Inchikey_classifications.csv")%>%
+  select(FeatureID, Final_Superclass)%>%
+  rename(Superclass = Final_Superclass)
+HILIC_classification <- read.csv("Input_Files/HILIC_Compound_Inchikey_classifications.csv") %>%
+  select(FeatureID, Final_Superclass)%>%
+  rename(Superclass = Final_Superclass)
+
+classification <- rbind(RP_classification, HILIC_classification) %>%
+  distinct()
+
+classification_count2 <- classification %>%
+  select(FeatureID, Superclass)%>%
+  distinct_all() %>%
+  filter(!is.na(Superclass))
+
+
+# Replace NA values in 'Class' column with "Other"
+classification$Superclass[is.na(classification$Superclass)] <- "Other" 
+
+# norm_matrix <- as.data.frame(t(norm_matrix))%>%
+#   rownames_to_column(var = "FeatureID")
+
+
+joined_data_classes <- left_join(norm_matrix_stand, classification, by = "FeatureID")%>%
+  pivot_longer(cols = 2:55, names_to = 'SampleName', values_to = 'intensities')
+
+Class_annotation_count <- joined_data_classes %>%
+  select(FeatureID, Superclass)%>%
+  distinct_all() %>%
+  filter(Superclass != "Other")
+
+Class_annotation_count2 <- joined_data_classes %>%
+  select(FeatureID, Superclass)%>%
+  distinct_all() %>%
+  filter(Superclass == "Other")
+
+# Group by sample and class, and calculate the average intensity for each class per sample
+class_intensity <- joined_data_classes %>%
+  group_by(SampleName, Superclass) %>%
+  summarise(avg_intensity = mean(intensities))
+
+class_matrix <- class_intensity %>%
+  pivot_wider(names_from = SampleName, values_from = avg_intensity)%>%
+  column_to_rownames(var = "Superclass")
+
+# Step 3: Make a heatmap showing the change in intensities of the classes for the different sample types
+
+
+# Heatmap of Classes: ---------
+# Initialize graphical device
+dev.off()
+
+# Set sampleID as row.names to annotate heatmap
+col_annot <- metadata %>% 
+  select(SampleName, Type)
+
+mapcolor <- colorRampPalette(brewer.pal(11, 'RdYlBu'))(100)[100:1]
+
+annot_colors <- list(
+  Type = c(Bare = "#3d1d0a", Biocrust = "#a8581b", Moss = "#116311"))
+
+row_annotations <- rownames(class_matrix)
+matrix <- as.data.frame(t(class_matrix))%>%
+  rownames_to_column("SampleName")%>%
+  right_join(col_annot, by="SampleName")%>%
+  arrange(Type)%>%
+  select(-Type)%>%
+  #select(-SampleID)%>%
+  column_to_rownames(var = "SampleName")
+new_matrix <- as.data.frame(t(matrix))
+
+col_annot <- metadata %>% 
+  select(SampleName, Type) %>%
+  column_to_rownames(var = "SampleName")
+figure_file <- file.path('Output_figures', 'FigureS4.png')
+png(figure_file, width = 2200, height = 1000, res = 300)
+p <- pheatmap(new_matrix,
+              clustering_distance_rows = 'correlation',
+              cluster_cols = F,
+              scale = 'row',
+              border_color = NA,
+              annotation_col = col_annot,
+              annotation_colors = annot_colors,
+              color = mapcolor,
+              gaps_col = c(18,36),
+              #cutree_rows = 3,
+              show_colnames = F,
+              main = 'Compound Superclass - Heatmap',
+              fontsize_row = 8,  # Adjust fontsize for row annotation
+              fontsize_col = 8,  # Adjust fontsize for column annotation
+              fontsize = 8
+)
+
+gridExtra::grid.arrange(p$gtable, vp=viewport(width=0.9, height=1))
+dev.off()
+
+
+
+
+#Pathways ####----
+RP_Kegg_pathways <- read.csv("Input_files/RP_Compound_Kegg_pathways.csv")
+HILIC_Kegg_pathways <- read.csv("Input_files/HILIC_Compound_Kegg_pathways.csv")
+KEGG_db <- read_xlsx("Input_files/KEGG_patheway_Classes_all.xlsx")
+
+RP_Kegg_pathways_all <- left_join(RP_Kegg_pathways, KEGG_db, by = "KEGG_pathway")%>%
+  select(FeatureID, KEGG_pathway, Pathway_Subclass, KEGG_id)
+HILIC_Kegg_pathways_all <- left_join(HILIC_Kegg_pathways, KEGG_db, by = "KEGG_pathway")%>%
+  select(FeatureID, KEGG_pathway, Pathway_Subclass, KEGG_id)
+
+Kegg_pathways_all <- rbind(RP_Kegg_pathways_all, HILIC_Kegg_pathways_all) 
+# Step 1: Group features based on Class categories
+# Replace NA values in 'Class' column with "Other"
+Kegg_pathways_all$KEGG_pathway[is.na(Kegg_pathways_all$KEGG_pathway)] <- "No Kegg annotation"
+
+
+# Step 2: Make an average of the intensities of each class categories per sample
+# Join the feature matrix with Compound_Classification dataframe to get the class information for each feature
+
+joined_data <- left_join(norm_matrix_stand, Kegg_pathways_all, by = "FeatureID")%>%
+  pivot_longer(cols = 2:55, names_to = 'SampleName', values_to = 'intensities')
+
+Kegg_count <- joined_data %>%
+  select(FeatureID, KEGG_id)%>%
+  distinct_all() %>%
+  filter(!is.na(KEGG_id))
+
+Kegg_count2 <- joined_data %>%
+  select(FeatureID, KEGG_id)%>%
+  distinct_all() %>%
+  filter(is.na(KEGG_id))
+
+# Group by sample and class, and calculate the average intensity for each class per sample
+pathway_intensity <- joined_data %>%
+  group_by(SampleName, Pathway_Subclass) %>%
+  summarise(avg_intensity = mean(intensities))
+pathway_intensity$Pathway_Subclass[is.na(pathway_intensity$Pathway_Subclass)] <- "No Kegg annotation"  
+
+pathway_matrix <- pathway_intensity %>%
+  pivot_wider(names_from = SampleName, values_from = avg_intensity)%>%
+  column_to_rownames(var = "Pathway_Subclass")
+
+
+# Step 3: Make a heatmap showing the change in intensities of the classes for the different sample types
+
+
+# Heatmap of Classes: 
+# Initialize graphical device
+dev.off()
+
+# Set sampleID as row.names to annotate heatmap
+col_annot <- metadata %>% 
+  select(SampleName, Type)
+
+mapcolor <- colorRampPalette(brewer.pal(11, 'RdYlBu'))(100)[100:1]
+
+annot_colors <- list(
+  Type = c(Bare = "#3d1d0a", Biocrust = "#a8581b", Moss = "#116311"))
+
+row_annotations <- rownames(pathway_matrix)
+matrix <- as.data.frame(t(pathway_matrix))%>%
+  rownames_to_column("SampleName")%>%
+  right_join(col_annot, by="SampleName")%>%
+  arrange(Type)%>%
+  select(-Type)%>%
+  column_to_rownames(var = "SampleName")
+new_matrix <- as.data.frame(t(matrix))
+
+col_annot <- metadata %>% 
+  select(SampleName, Type) %>%
+  column_to_rownames(var = "SampleName")
+figure_file <- file.path('Output_figures', 'FigS5.png')
+png(figure_file, width = 2200, height = 1000, res = 300)
+p <- pheatmap(new_matrix,
+         clustering_distance_rows = 'correlation',
+         cluster_cols = F,
+         scale = 'row',
+         annotation_col = col_annot,
+         annotation_colors = annot_colors,
+         color = mapcolor,
+         gaps_col = c(18,36),
+         #cutree_rows = 5,
+         main = 'KEGG Pathway Classes - Heatmap',
+         fontsize_row = 8,  # Adjust fontsize for row annotation
+         fontsize_col = 8,  # Adjust fontsize for column annotation
+         fontsize = 8,
+         border_color = NA,
+         show_colnames = F
+)
+gridExtra::grid.arrange(p$gtable, vp=viewport(width=0.9, height=1))
+dev.off()
+
+
+#gridExtra::grid.arrange(p$gtable, vp=viewport(width=0.9, height=1))
+
